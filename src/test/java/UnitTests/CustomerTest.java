f5773ecc32f112eaf250bce3e7195d2cdecfed70
Serg2DFX <Serg2DFX>
2017-07-10 16:46:55 +0300

    First commit

--- src/test/java/UnitTests/CustomerTest.java
+++ src/test/java/UnitTests/CustomerTest.java
@@ -1,6 +1,9 @@
 package UnitTests;
 
 import com.scrumtrek.simplestore.Customer;
+import com.scrumtrek.simplestore.Movie;
+import com.scrumtrek.simplestore.PriceCodes;
+import com.scrumtrek.simplestore.Rental;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -24,5 +27,46 @@ public class CustomerTest {
         Assert.assertTrue(result.contains("Rental record for " + customerName));
         Assert.assertTrue(result.contains("Amount owed is " + 0));
         Assert.assertTrue(result.contains("You earned " + 0 + " frequent renter points."));
+    }
+
+    @Test()
+    public void ShouldProvideCustomerStatementWhenThreeMoviesAdded()
+    {
+        String customerName = "customer name";
+        Customer customer = new Customer(customerName);
+
+        Movie movie1 = new Movie("Rental 1 (new release)", PriceCodes.NewRelease);
+        customer.addRental(new Rental(movie1, 3));
+
+        Movie movie2 = new Movie("Rental 1 (new children)", PriceCodes.Childrens);
+        customer.addRental(new Rental(movie2, 2));
+
+        Movie movie3 = new Movie("Rental 1 (new regular)", PriceCodes.Regular);
+        customer.addRental(new Rental(movie3, 1));
+
+
+        String result = customer.Statement();
+        Assert.assertTrue(result.contains("Rental record for " + customerName));
+        Assert.assertTrue(result.contains("Amount owed is " + 12.5));
+        Assert.assertTrue(result.contains("You earned " + 4 + " frequent renter points."));
+    }
+
+    @Test()
+    public void ShouldProvideCustomerStatementWhenTwoMoviesAdded()
+    {
+        String customerName = "customer name";
+        Customer customer = new Customer(customerName);
+
+        Movie movie2 = new Movie("Rental 1 (new children)", PriceCodes.Childrens);
+        customer.addRental(new Rental(movie2, 4));
+
+        Movie movie3 = new Movie("Rental 1 (new regular)", PriceCodes.Regular);
+        customer.addRental(new Rental(movie3, 3));
+
+
+        String result = customer.Statement();
+        Assert.assertTrue(result.contains("Rental record for " + customerName));
+        Assert.assertTrue(result.contains("Amount owed is " + 5));
+        Assert.assertTrue(result.contains("You earned " + 2 + " frequent renter points."));
     }
 }
